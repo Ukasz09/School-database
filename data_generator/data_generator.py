ba4345c8d7a787b8f3b9ad1a919c221a5c2da9d0
Ukasz09 <246703@student.pwr.edu.pl>
2020-05-15 22:12:53 +0200

    Refactor data generator, change some name from pl to eng

--- data_generator/data_generator.py
+++ data_generator/data_generator.py
@@ -9,6 +9,7 @@ MALE_NAME_CSV = "csv/male_names.csv"
 SURNAMES_CSV = "csv/surnames.csv"
 CITIES_CSV = "csv/places_usa.csv"
 SUBJECTS_CSV = "csv/subjects.csv"
+CLASS_PROFILES_CSV = "csv/class_profiles.csv"
 
 # output
 STUDENTS_CSV = "csv/students.csv"
@@ -17,47 +18,41 @@ CLASSES_CSV = "csv/classes.csv"
 MARKS_CSV = "csv/grades.csv"
 TEACHING_CSV = "csv/teaching.csv"
 
-csv_delimiter = ","
-class_prefixes = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"]
-class_sufixes = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
-min_hourly_rate = 17
-teacher_max_hourly_rate = 35
-max_weekly_pensum = 40
-general_class_profile = "ogolnoksztalcacy"
-class_profiles = ["matematyczno-fizyczny", "humanistyczny", "matematyczno-geografizcny",
-                  "informatyczny", "matematyczny", "biologiczny", "chamiczny", "biologiczno-chemiczny", "sportowy",
-                  "artystyczny", "muzyczny"]
-marks = [1, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5]
+# others
+CSV_DELIMITER = ","
+CLASS_PREFIXES = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"]
+CLASS_SUFFIXES = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
+GENERAL_CLASS_PROFILE = "general education"
+GRADES = [1, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5]
+MIN_HOURLY_RATE = 17
+MAX_HOURLY_RATE = 35
+MAX_WEEKLY_TEACHING_LOAD = 40
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
-def csv_data_to_list(csv_filepath, data_from_columns=1, delim=",", quote='"'):
+def csv_data_to_list(csv_filepath, data_columns=1, delimiter=",", quotechar='"'):
     with open(csv_filepath, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=delim, quotechar=quote)
-        result = []
-        for row in reader:
-            result.append(row[data_from_columns])
-    return result
+        reader = csv.reader(csvfile, delimiter=delimiter, quotechar=quotechar)
+        return [row[data_columns] for row in reader]
 
 
-def get_csv_file_to_write(csv_filepath, delim=","):
-    csv_file = open(csv_filepath, 'w', newline='')
-    return csv_file
+def get_csv_write_file(csv_filepath):
+    return open(csv_filepath, 'w', newline='')
 
 
-def get_random_object(obj_list):
-    index = random.randint(0, len(obj_list) - 1)
-    return obj_list[index]
+def rand_elem(elem_list):
+    index = random.randint(0, len(elem_list) - 1)
+    return elem_list[index]
 
 
-def get_random_object_with_removing(obj_list):
-    index = random.randint(0, len(obj_list) - 1)
-    obj = obj_list[index]
-    del obj_list[index]
+def remove_and_return_rand_elem(elem_list):
+    index = random.randint(0, len(elem_list) - 1)
+    obj = elem_list[index]
+    del elem_list[index]
     return obj
 
 
-def get_random_date(start, end):
+def rand_date(start, end):
     delta = end - start
     random_days = random.randrange(delta.days)
     return start + timedelta(days=random_days)
@@ -67,16 +62,14 @@ def format_date(date):
     return datetime.strftime(date, "%d-%m-%Y")
 
 
-def get_random_class_symbol():
-    return get_random_object(class_prefixes) + get_random_object(class_sufixes)
+def rand_class_symbol():
+    return rand_elem(CLASS_PREFIXES) + rand_elem(CLASS_SUFFIXES)
 
 
-def get_random_profile(prop_for_general_profile):
-    rand_numb = random.randint(0, 100)
-    if rand_numb <= 100 * prop_for_general_profile:
-        return general_class_profile
-    else:
-        return get_random_object(class_profiles)
+def rand_profile(probability_of_general_profile):
+    if random.randint(0, 100) <= 100 * probability_of_general_profile:
+        return GENERAL_CLASS_PROFILE
+    return rand_elem(csv_data_to_list(CLASS_PROFILES_CSV))
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -84,96 +77,107 @@ female_names = csv_data_to_list(FEMALE_NAME_CSV)
 male_names = csv_data_to_list(MALE_NAME_CSV)
 surnames = csv_data_to_list(SURNAMES_CSV)
 cities = csv_data_to_list(CITIES_CSV)
+all_possible_classes = [pref + suf for pref, suf in product(CLASS_PREFIXES, CLASS_SUFFIXES)]
+
+
+def rand_student_birth_date():
+    start_date = datetime.strptime("01-01-1990", "%d-%m-%Y")
+    end_date = datetime.now() - timedelta(days=18 * 365)
+    return rand_date(start_date, end_date)
 
 
 def generate_student_data(amount):
-    csv_file = get_csv_file_to_write(STUDENTS_CSV)
-    csv_writer = csv.writer(csv_file, delimiter=csv_delimiter)
-    start_date = datetime.strptime("01-01-1990", "%d-%m-%Y")
-    end_date = datetime.now() - timedelta(days=18 * 365)
+    csv_file = get_csv_write_file(STUDENTS_CSV)
+    csv_writer = csv.writer(csv_file, delimiter=CSV_DELIMITER)
     indexes = []
     for i in range(amount):
         sex = 'W' if random.randint(0, 1) == 0 else 'M'
-        name = get_random_object(female_names) if sex == "W" else get_random_object(male_names)
-        surname = get_random_object(surnames)
-        birth_date = format_date(get_random_date(start_date, end_date))
-        location = get_random_object(cities)
-        class_symbol = get_random_class_symbol()
-        csv_writer.writerow([i, surname, name, birth_date, sex, class_symbol, location])
+        name = rand_elem(female_names) if sex == "W" else rand_elem(male_names)
+        birth_date = format_date(rand_student_birth_date())
+        csv_writer.writerow([i, rand_elem(surnames), name, birth_date, sex, rand_class_symbol(), rand_elem(cities)])
         indexes.append(i)
     csv_file.close()
     return indexes
 
 
-def generate_teacher_data(amount):
-    csv_file = get_csv_file_to_write(TEACHERS_CSV)
-    csv_writer = csv.writer(csv_file, delimiter=csv_delimiter)
+def rand_teacher_birth_date():
     birth_start_date = datetime.strptime("01-01-1960", "%d-%m-%Y")
     birth_end_date = datetime.now() - timedelta(days=20 * 365)
+    return rand_date(birth_start_date, birth_end_date)
+
+
+def rand_teacher_employment_date(birth_date):
     employment_end_date = datetime.now() - timedelta(days=2 * 365)
+    return rand_date(birth_date + timedelta(days=18 * 365), employment_end_date)
+
+
+def generate_teacher_data(amount):
+    csv_file = get_csv_write_file(TEACHERS_CSV)
+    csv_writer = csv.writer(csv_file, delimiter=CSV_DELIMITER)
     indexes = []
     for i in range(amount):
         sex = 'W' if random.randint(0, 1) == 0 else 'M'
-        name = get_random_object(female_names) if sex == "W" else get_random_object(male_names)
-        surname = get_random_object(surnames)
-        birth_date = get_random_date(birth_start_date, birth_end_date)
-        employment_date = format_date(get_random_date(birth_date + timedelta(days=18 * 365), employment_end_date))
-        hourly_rate = round(random.uniform(min_hourly_rate, teacher_max_hourly_rate), 2)
-        pensum = random.randint(0, max_weekly_pensum)
-        csv_writer.writerow([i, surname, name, employment_date, format_date(birth_date), sex, hourly_rate, pensum])
+        name = rand_elem(female_names) if sex == "W" else rand_elem(male_names)
+        birth_date = rand_teacher_birth_date()
+        employment_date = format_date(rand_teacher_employment_date(birth_date))
+        hourly_rate = round(random.uniform(MIN_HOURLY_RATE, MAX_HOURLY_RATE), 2)
+        teaching_load = random.randint(0, MAX_WEEKLY_TEACHING_LOAD)
+        row = [i, rand_elem(surnames), name, employment_date, format_date(birth_date), sex, hourly_rate, teaching_load]
+        csv_writer.writerow(row)
         indexes.append(i)
     csv_file.close()
     return indexes
 
 
 def generate_school_classes(teachers_id_list, probability_of_general_profile):
-    csv_file = get_csv_file_to_write(CLASSES_CSV)
-    csv_writer = csv.writer(csv_file, delimiter=csv_delimiter)
-    all_symbols = []
-    # all possible classes
-    for pref, suf in product(class_prefixes, class_sufixes):
-        all_symbols.append(pref + suf)
-    for symbol in all_symbols:
-        profile = get_random_profile(probability_of_general_profile)
-        tutor_id = get_random_object(teachers_id_list)
+    csv_file = get_csv_write_file(CLASSES_CSV)
+    csv_writer = csv.writer(csv_file, delimiter=CSV_DELIMITER)
+    for symbol in all_possible_classes:
+        profile = rand_profile(probability_of_general_profile)
+        tutor_id = rand_elem(teachers_id_list)
         csv_writer.writerow([symbol, profile, tutor_id])
     csv_file.close()
-    return all_symbols
 
 
-def generate_marks(amount, subjects_symbols_list, teacher_id_list, students_id_list):
-    csv_file = get_csv_file_to_write(MARKS_CSV)
-    csv_writer = csv.writer(csv_file, delimiter=csv_delimiter)
+def rand_grade_date():
     start_date = datetime.strptime("01-01-2019", "%d-%m-%Y")
     end_date = datetime.strptime("31-12-2019", "%d-%m-%Y")
+    return rand_date(start_date, end_date)
+
+
+def generate_marks(amount, subj_symb_list, teacher_id_list, stud_id_list):
+    csv_file = get_csv_write_file(MARKS_CSV)
+    csv_writer = csv.writer(csv_file, delimiter=CSV_DELIMITER)
     for i in range(amount):
-        stud_id = get_random_object(students_id_list)
-        subj_id = get_random_object(subjects_symbols_list)
-        teach_id = get_random_object(teacher_id_list)
-        mark = get_random_object(marks)
-        date = format_date(get_random_date(start_date, end_date))
-        csv_writer.writerow([stud_id, subj_id, teach_id, mark, date])
+        date = format_date(rand_grade_date())
+        row = [rand_elem(stud_id_list), rand_elem(subj_symb_list), rand_elem(teacher_id_list), rand_elem(GRADES), date]
+        csv_writer.writerow(row)
     csv_file.close()
+
+
+def teacher_with_all_subjects_dict(subject_id_list):
+    teacher_with_subjects = {}
+    for teacher_id in teacher_id_list:
+        teacher_with_subjects[teacher_id] = subject_id_list.copy()
+    return teacher_with_subjects
 
 
 def generate_teaching(amount, teacher_id_list, subject_id_list):
-    csv_file = get_csv_file_to_write(TEACHING_CSV)
-    csv_writer = csv.writer(csv_file, delimiter=csv_delimiter)
-    teacher_with_avail_subj = {}
-    for id in teacher_id_list:
-        teacher_with_avail_subj[id] = subject_id_list.copy()
+    csv_file = get_csv_write_file(TEACHING_CSV)
+    csv_writer = csv.writer(csv_file, delimiter=CSV_DELIMITER)
+    teacher_dict = teacher_with_all_subjects_dict(subject_id_list)
     for i in range(amount):
-        teacher_id = get_random_object(teacher_id_list)
-        subject_id = get_random_object_with_removing(teacher_with_avail_subj.get(teacher_id))
-        hours_per_week = random.randint(1, 20)
+        teacher_id = rand_elem(teacher_id_list)
+        subject_id = remove_and_return_rand_elem(teacher_dict.get(teacher_id))
+        hours_per_week = random.randint(1, int(MAX_WEEKLY_TEACHING_LOAD / 3))
         csv_writer.writerow([teacher_id, subject_id, hours_per_week])
     csv_file.close()
 
 
 if __name__ == "__main__":
-    subject_id_list = csv_data_to_list(SUBJECTS_CSV, data_from_columns=0)
+    subject_id_list = csv_data_to_list(SUBJECTS_CSV, data_columns=0)
     stud_id_list = generate_student_data(600)
     teacher_id_list = generate_teacher_data(50)
-    class_symbols = generate_school_classes(teacher_id_list, 0.3)
+    generate_school_classes(teacher_id_list, 0.3)
     generate_marks(200, subject_id_list, teacher_id_list, stud_id_list)
     generate_teaching(150, teacher_id_list, subject_id_list)
